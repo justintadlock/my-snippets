3e37e785a3b8b6c5f44ea2cff26f15b5b7d84fb7
Justin Tadlock <justin@justintadlock.com>
2013-10-07 20:04:39 -0500

    Update plugin header info.

--- my-snippets.php
+++ my-snippets.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: My Snippetss
- * Plugin URI: http://justintadlock.com/archives/2009/12/03/my-snippets-wordpress-plugin
+ * Plugin Name: My Snippets
+ * Plugin URI: http://themehybrid.com/plugins/my-snippets
  * Description: Add custom snippets to your widget areas (sidebars) on a post-by-post basis.
  * Version: 0.2.0-alpha
  * Author: Justin Tadlock
@@ -11,11 +11,23 @@
  * that allows users to add custom metadata to the post. This metadata is then displayed for
  * the singular view of the post on the frontend of the site by using the My Snippets widget.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
  *
- * @package MySnippets
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @package   MySnippets
+ * @version   0.2.0
+ * @since     0.1.0
+ * @author    Justin Tadlock <justin@justintadlock.com>
+ * @copyright Copyright (c) 2009 - 2013, Justin Tadlock
+ * @link      http://themehybrid.com/plugins/my-snippets
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 /* Set constant path to the my_snippets plugin directory. */
