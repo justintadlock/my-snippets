183ead6eece99debe86cacdece11a9da111425d6
Justin Tadlock <justin@justintadlock.com>
2013-10-07 20:43:21 -0500

    Recode the meta box.

--- admin/meta-box.php
+++ admin/meta-box.php
@@ -1,103 +1,145 @@
 <?php
 
-/* Create the post meta box. */
-add_action( 'admin_menu', 'my_snippets_create_post_meta_box' );
+final class My_Snippets_Meta_Boxes {
 
-/* Save the post metadata. */
-add_action( 'save_post', 'my_snippets_save_post_meta_box', 10, 2 );
+	/**
+	 * Holds the instance of this class.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    object
+	 */
+	private static $instance;
 
-/**
- * Adds the meta boxes to the edit post/page screen in the WordPress admin. Users can also
- * filter the `edit_my_snippet_capability` to create a custom capability for adding My Snippetss.
- *
- * @since 0.1
- */
-function my_snippets_create_post_meta_box() {
-	global $theme_name;
+	/**
+	 * Sets up the needed actions for adding and saving the meta boxes.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' )        );
+		add_action( 'save_post',      array( $this, 'save_post'      ), 10, 2 );
+	}
 
-	$capability = apply_filters( 'edit_snippet_capability', false );
+	/**
+	 * Adds the meta box.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function add_meta_boxes( $post ) {
 
-	if (  ( !$capability ) || ( $capability && current_user_can( $capability ) ) ) {
-		add_meta_box( 'my-snippets-post-meta-box', sprintf( __('Snippet %1$s settings', 'my-snippets'), 'post' ), 'my_snippets_post_meta_box', 'post', 'normal', 'high' );
-		add_meta_box( 'my-snippets-page-meta-box', sprintf( __('Snippet %1$s settings', 'my-snippets'), 'page' ), 'my_snippets_post_meta_box', 'page', 'normal', 'high' );
+		add_meta_box( 
+			'my-snippets', 
+			__( 'My Snippets', 'my-snippets' ), 
+			array( $this, 'snippet_meta_box' ), 
+			$post->post_type, 
+			'normal', 
+			'low' 
+		);
+	}
+
+	/**
+	 * Displays the "my snippets" meta box.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  object  $object  Current post object.
+	 * @param  array   $box
+	 * @return void
+	 */
+	public function details_meta_box( $post, $box ) { 
+
+		wp_nonce_field( plugin_basename( __FILE__ ), 'my_snippets_meta_nonce' );
+
+		$snippet_title   = get_post_meta( $post->ID, 'Snippet Title', true );
+		$snippet_content = get_post_meta( $post->ID, 'Snippet', true ); ?>
+
+		<p>
+			<label for="snippet-title"><?php _e( 'Snippet Title', 'my-snippets' ); ?></label> 
+			<input class="widefat" type="text" name="snippet-title" id="snippet-title" value="<?php echo esc_attr( $snippet_title ); ?>" />
+		</p>
+
+		<p>
+			<label for="snippet"><?php _e( 'Snippet Content', 'my-snippets'); ?></label>
+			<textarea class="widefat" name="snippet-content" id="snippet-content" cols="60" rows="4"><?php echo esc_textarea( $snippet_content ); ?></textarea>
+			<span class="description"><?php _e( 'Add text, <acronym title="Hypertext Markup Language">HTML</acronym>, and/or shortcodes.', 'my-snippets' ); ?></span>
+		</p><?php
+	}
+
+	/**
+	 * Saves the custom post meta for the meta boxes.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  int     $post_id
+	 * @param  object  $post
+	 * @return void
+	 */
+	public function save_post( $post_id, $post ) {
+
+		/* Verify the nonce. */
+		if ( !isset( $_POST['my_snippets_meta_nonce'] ) || !wp_verify_nonce( $_POST['my_snippets_meta_nonce'], plugin_basename( __FILE__ ) ) )
+			return;
+
+		/* Get the post type object. */
+		$post_type = get_post_type_object( $post->post_type );
+
+		/* Check if the current user has permission to edit the post. */
+		if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+			return $post_id;
+
+		/* Don't save if the post is only a revision. */
+		if ( 'revision' == $post->post_type )
+			return;
+
+		$meta = array(
+			'Snippet Title' => strip_tags( $_POST['snippet-title'] )
+		);
+
+		if ( current_user_can('unfiltered_html') )
+			$meta['Snippet'] =  $_POST['snippet-content'];
+		else
+			$meta['Snippet'] = stripslashes( wp_filter_post_kses( addslashes( $_POST['snippet-content'] ) ) );
+
+		foreach ( $meta as $meta_key => $new_meta_value ) {
+
+			/* Get the meta value of the custom field key. */
+			$meta_value = get_post_meta( $post_id, $meta_key, true );
+
+			/* If a new meta value was added and there was no previous value, add it. */
+			if ( $new_meta_value && '' == $meta_value )
+				add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+			/* If the new meta value does not match the old value, update it. */
+			elseif ( $new_meta_value && $new_meta_value != $meta_value )
+				update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+			/* If there is no new meta value but an old value exists, delete it. */
+			elseif ( '' == $new_meta_value && $meta_value )
+				delete_post_meta( $post_id, $meta_key, $meta_value );
+		}
+	}
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return object
+	 */
+	public static function get_instance() {
+
+		if ( !self::$instance )
+			self::$instance = new self;
+
+		return self::$instance;
 	}
 }
 
-/**
- * Displays the post meta box, which includes the Snippet Title and Snippet Content fields.
- *
- * @since 0.1
- */
-function my_snippets_post_meta_box() {
-	global $post;
-
-	$my_snippet_title = get_post_meta( $post->ID, 'Snippet Title', true );
-	$my_snippet = get_post_meta( $post->ID, 'Snippet', true ); ?>
-
-	<p>
-		<label for="snippet-title"><?php _e('<strong>Snippet Title:</strong> (Overwrites the widget title.)', 'my-snippets'); ?></label>
-		<br />
-		<input type="text" name="snippet-title" id="snippet-title" value="<?php echo wp_specialchars( $my_snippet_title, 1 ); ?>" size="30" tabindex="30" style="width: 99%;" />
-	</p>
-
-	<p>
-		<label for="snippet"><?php _e('<strong>Snippet:</strong> (Add text, <acronym title="Hypertext Markup Language">HTML</acronym>, and/or shortcodes.)', 'my-snippets'); ?></label>
-		<br />
-		<textarea name="snippet" id="snippet" cols="60" rows="4" style="width: 99%;"><?php echo wp_specialchars( $my_snippet, 1 ); ?></textarea>
-		<input type="hidden" name="my_snippets_noncename" id="my_snippets_noncename" value="<?php echo wp_create_nonce( plugin_basename( __FILE__ ) ); ?>" />
-	</p>
-
-	<?php
-}
-
-/**
- * Saves the post metadata added through the My Snippetss settings fields.
- *
- * @since 0.1
- */
-function my_snippets_save_post_meta_box( $post_id, $post ) {
-
-	/* Only allow users that can edit the current post to submit data. */
-	if ( 'post' == $post->post_type && !current_user_can( 'edit_post', $post_id ) )
-		return;
-
-	/* Only allow users that can edit the current page to submit data. */
-	elseif ( 'page' == $post->post_type && !current_user_can( 'edit_page', $post_id ) )
-		return;
-
-	/* Don't save if the post is only a revision. */
-	if ( 'revision' == $post->post_type )
-		return;
-
-	/* Verify the post form. */
-	if ( !wp_verify_nonce( $_POST['my_snippets_noncename'], plugin_basename( __FILE__ ) ) )
-		return $post_id;
-
-	/* Loop through metadata, checking each option. */
-	foreach ( array( 'snippet-title' => 'Snippet Title', 'snippet' => 'Snippet' ) as $key => $value ) {
-
-		/* Current my_snippet. */
-		$old_meta = get_post_meta( $post_id, $value, true );
-
-		/* Posted my_snippet. */
-		$new_meta = $_POST[$key];
-
-		/* Only allow 'unfiltered_html' in the my_snippet for users that have that capability. */
-		if ( !current_user_can( 'unfiltered_html' ) )
-			$new_meta = stripslashes( wp_filter_post_kses( $new_meta ) );
-
-		/* If there is no snippet but one posted, add it. */
-		if ( $new_meta && '' == $old_meta )
-			add_post_meta( $post_id, $value, $new_meta, true );
-
-		/* If the new snippet is different than the old one, update it. */
-		elseif ( $old_meta != $new_meta )
-			update_post_meta( $post_id, $value, $new_meta );
-
-		/* If the posted snippet is blank, delete the old my_snippet. */
-		if ( '' == $new_meta && $old_meta )
-			delete_post_meta( $post_id, $value );
-	}
-}
+My_Snippets_Meta_Boxes::get_instance();
 
 ?>
